0cc9397e40209feb74dc97b7b143dcb4fa0fffe6
Derek Leggo <dleggo@protonmail.com>
2024-10-18 09:38:23 -0600

    Base tree

--- main.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd helloDesktop
-
-git clone https://github.com/helloSystem/Filer.git
-git clone https://github.com/helloSystem/Menu.git
-git clone https://github.com/helloSystem/launch.git
-git clone https://github.com/helloSystem/Utilities.git
-
