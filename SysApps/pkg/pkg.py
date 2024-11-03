c02964ba9f289d5da036f3ead5623b1fe5e3b1a2
Derek Leggo <dleggo@protonmail.com>
2024-11-03 14:32:27 -0700

    More work on pkg

--- SysApps/pkg/pkg.py
+++ SysApps/pkg/pkg.py
@@ -1,23 +1,29 @@
 #!/bin/python3
 
 import os
-import tempfile
+from tempfile import TemporaryDirectory
 import tomllib
 
-def get_info(path: str) -> dict:
+def get_pkg_info(path: str) -> dict:
 	assert os.path.exists(path), "Package not found!"
 	assert os.path.exist(path + "/pkg-info") # FIXME
 	with open(path + "/pkg-info", "rb") as f:
 		info = tomllib.load(f)
 	return info
 
+def get_deb_info(path: str) -> dict:
+	assert os.path.exists(path), "Supplied path does not exist!"
+
 
 # path should point to a .deb file
-def install(path: str):
+def install_deb(path: str, root: str):
 	assert os.path.exists(path), "Supplied path does not exist!"
+	assert path.lower()[-4:] == ".deb", "Path does not point to a .deb file!"
 	# create pkg directory
-	# extract pkg
-	# extract metadata
+	with TemporaryDirectory() as tmpdir:
+		# extract pkg
+		os.system(f"dpkg -x {path} {tmpdir}")
+		# extract metadata
 
 if __name__ == "__main__":
-	install("/home/derek/Code/Dalix/Resources/bwrapchroot.deb")
+	install_deb("/home/derek/Code/Dalix/Resources/bwrap.deb", "/home/derek/Code/Dalix/Tests")
