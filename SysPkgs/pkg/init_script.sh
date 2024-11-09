46f6291efa13f6411bac5c8254647740b65597bb
Derek Leggo <dleggo@protonmail.com>
2024-11-09 14:45:30 -0700

    Add micro to install packages

--- SysPkgs/pkg/init_script.sh
+++ SysPkgs/pkg/init_script.sh
@@ -7,7 +7,7 @@ mkdir /Applications
 mkdir /Volumes
 mkdir /Users
 
-apt install sudo python3 python3-tomli-w python3-termcolor python3-packaging -y
+apt install sudo python3 python3-tomli-w python3-termcolor python3-packaging micro -y
 
 /sbin/adduser user --home /Users/user <<EOF
 1234
