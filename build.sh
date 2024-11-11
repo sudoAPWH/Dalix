0017bbf6ccfb6a70ab685f5b1b15d4360dcef389
Derek Leggo <dleggo@protonmail.com>
2024-11-10 20:50:55 -0700

    Minor changes

--- build.sh
+++ build.sh
@@ -12,6 +12,8 @@ mkdir -p build/Applications
 cp SysPkgs/pkg/full.py build/usr/bin/pkg
 cp control build/DEBIAN/control
 
+dpkg -x Resources/bwrap.deb build
+
 dpkg-deb --build build
 mv build.deb dalixos-base.deb
 mv dalixos-base.deb Resources/
