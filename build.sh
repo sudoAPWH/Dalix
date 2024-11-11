c2b496348672b0e9050dee3086ec5532f4be6206
Derek Leggo <dleggo@protonmail.com>
2024-11-10 20:39:57 -0700

    Fix bad path in build.sh and add built file to gitignore

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ cp SysPkgs/pkg/full.py build/usr/bin/pkg
 cp control build/DEBIAN/control
 
 dpkg-deb --build build
-mv build.deb pkg.deb
-mv pkg.deb ../../Resources
+mv build.deb dalixos-base.deb
+mv dalixos-base.deb Resources/
 
 rm -R build
