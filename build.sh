1ed43c7ea863d948d42f6fd3074546ea45e4939e
Derek Leggo <dleggo@protonmail.com>
2024-11-10 20:30:32 -0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This is the build script for pkg
+mkdir -p build/usr/bin
+mkdir -p build/DEBIAN
+
+cp full.py build/usr/bin/pkg
+cp control build/DEBIAN/control
+
+dpkg-deb --build build
+mv build.deb pkg.deb
+mv pkg.deb ../../Resources
+
+rm -R build
