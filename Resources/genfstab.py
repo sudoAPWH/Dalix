13c88fba81dcd2a3911308f24b9710660000dca3
Derek Leggo <dleggo@protonmail.com>
2024-11-15 16:52:11 -0700

    Add genfstab.py

---
+++ Resources/genfstab.py
@@ -0,0 +1,18 @@
+#!/bin/python
+
+import sys, subprocess
+# args should have len(3)
+assert len(sys.argv) == 3, "Not enough arguments"
+
+UUID1 = subprocess.check_output(f"findmnt {sys.argv[1]} -o UUID", shell=True).decode("utf-8")
+UUID2 = subprocess.check_output(f"findmnt {sys.argv[2]} -o UUID", shell=True).decode("utf-8")
+
+UUID1 = UUID1.replace("UUID", "")
+UUID2 = UUID2.replace("UUID", "")
+UUID1 = UUID1.lstrip().rstrip()
+UUID2 = UUID2.lstrip().rstrip()
+
+# root partition
+print(f"UUID={UUID1}    /   ext4    rw,relatime 0 1")
+# boot partition
+print(f"UUID={UUID2}    /boot   vfat    rw,relatime,fmask=0022,dmask=0022,codepage=437,iocharset=ascii,shortname=mixed,utf8,errors=remount-ro   0 2")
