dfd55a9f0189c5a15a55e1d6d957627fd22ee80d
Derek Leggo <dleggo@protonmail.com>
2024-11-15 22:59:55 -0700

    Make makeiso.sh MUCH faster

--- makebase.sh
+++ makebase.sh
@@ -4,7 +4,7 @@ sudo rm -Rf Tests/base
 sudo debootstrap unstable Tests/base http://deb.debian.org/debian/
 sudo rm -Rf Tests/base/boot
 sudo arch-chroot Tests/base <<EOF
-
+apt install -y sudo python3 python3-tomli-w python3-termcolor micro python3-pip bubblewrap
 apt install -y network-manager ifupdown isc-dhcp-client pppoeconf wpasupplicant wireless-tools iw iproute2
 apt install -y iptables nftables iputils-ping iputils-arping iputils-tracepath ethtool mtr nmap
 apt install -y tcptrace ntopng dnsutils dlint dnstracer
