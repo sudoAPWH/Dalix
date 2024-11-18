355735824cff0220874a70f505c4016313050c06
Derek Leggo <dleggo@protonmail.com>
2024-11-17 20:11:21 -0700

    Improve setup

--- makebase.sh
+++ makebase.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 sudo rm -Rf Tests/base
-sudo debootstrap stable Tests/base http://deb.debian.org/debian/
+sudo debootstrap unstable Tests/base http://deb.debian.org/debian/
 sudo rm -Rf Tests/base/boot
 sudo arch-chroot Tests/base <<EOF
 apt install -y sudo python3 python3-tomli-w python3-termcolor micro python3-pip bubblewrap
