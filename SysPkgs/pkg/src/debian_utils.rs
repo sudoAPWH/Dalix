8e413c1f992eeb2910caddd06e4ad054b3398ecf
Derek Leggo <dleggo@protonmail.com>
2024-11-21 17:21:03 -0700

    Work on debian utils

--- SysPkgs/pkg/src/debian_utils.rs
+++ SysPkgs/pkg/src/debian_utils.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 use crate::system;
 use std::process::ExitStatus;
+use temp_dir::TempDir;
 
 pub struct DebPkg {
 	name: String,
@@ -37,16 +38,36 @@ impl DebFile<'_> {
 /// # Returns
 ///
 /// A boolean indicating the success (`true`) or failure (`false`) of the extraction process.
-pub fn extract_deb_full(d: &DebPkg, out: &Path) -> bool {
+///
+/// # Example
+/// ```
+/// let deb = DebPkg {
+///     name: "package-name".to_string(),
+///     version: "1.0".to_string(),
+///     arch: "amd64".to_string(),
+///     deps: "libwhatever-dev".to_string(),
+///     description: "Package Description".to_string(),
+///     maintainer: "Package Maintainer".to_string(),
+///     path: Path::new("/path/to/my-package.deb"),
+/// };
+///
+/// if extract_deb_full(&deb, Path::new("/path/to/extracted")) {
+///     println!("Extraction successful!");
+/// } else {
+///     println!("Extraction failed!");
+/// }
+/// ```
+pub fn extract_deb_full(d: &DebFile, out: &Path) -> bool {
 	system::cmd(&format!("dpkg-deb -R {} {}", d.path.display(), out.display()))
 }
 
 
-pub fn extract_deb(d: &DebPkg, out: &Path) -> bool {
+pub fn extract_deb(d: &DebFile, out: &Path) -> bool {
 	system::cmd(&format!("dpkg-deb -R {} {}", d.path.display(), out.display()))
 }
 
 /// Gets a DebPkg struct from a deb file
 pub fn extract_info(deb: &DebFile) {
-	
+	let dir = TempDir::new().unwrap();
+	extract_deb_full(deb, dir.path());
 }
