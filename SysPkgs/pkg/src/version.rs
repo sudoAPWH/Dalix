17c5c77a74ce404a0f8daa6a132fae891b24f901
Derek Leggo <dleggo@protonmail.com>
2024-11-22 11:40:37 -0700

    Revamped version class

--- SysPkgs/pkg/src/version.rs
+++ SysPkgs/pkg/src/version.rs
@@ -3,13 +3,13 @@ use std::cmp::PartialEq;
 use rust_apt::util;
 
 #[derive(Debug)]
-pub struct Version<'a> {
-    pub s: &'a str // s = string
+pub struct Version {
+    pub s: String // s = string
 }
 
-impl PartialEq for Version<'_> {
+impl PartialEq for Version {
     fn eq(&self, other: &Version) -> bool {
-        util::cmp_versions(self.s, other.s) == Ordering::Equal
+        util::cmp_versions(self.s.as_str(), other.s.as_str()) == Ordering::Equal
     }
     fn ne(&self, other: &Version) -> bool {
         !self.eq(other)
@@ -17,25 +17,25 @@ impl PartialEq for Version<'_> {
 }
 
 #[allow(dead_code)]
-impl Version<'_> {
-    pub fn new(s: &str) -> Version {
+impl Version {
+    pub fn new(s: String) -> Version {
         Version { s }
     }
 
     pub fn gt(&self, other: &Version) -> bool {
-        util::cmp_versions(self.s, other.s) == Ordering::Less
+        util::cmp_versions(self.s.as_str(), other.s.as_str()) == Ordering::Less
     }
 
     pub fn ge(&self, other: &Version) -> bool {
-        util::cmp_versions(self.s, other.s) != Ordering::Greater
+        util::cmp_versions(self.s.as_str(), other.s.as_str()) != Ordering::Greater
     }
 
     pub fn lt(&self, other: &Version) -> bool {
-        util::cmp_versions(self.s, other.s) == Ordering::Greater
+        util::cmp_versions(self.s.as_str(), other.s.as_str()) == Ordering::Greater
     }
 
     pub fn le(&self, other: &Version) -> bool {
-        util::cmp_versions(self.s, other.s) != Ordering::Less
+        util::cmp_versions(self.s.as_str(), other.s.as_str()) != Ordering::Less
     }
 }
 
@@ -45,41 +45,41 @@ mod tests {
 
     #[test]
     fn test_version_eq() {
-        let v = Version::new("1.2.3");
-        assert_eq!(v, Version { s: "1.2.3" });
+        let v = Version::new("1.2.3".to_string());
+        assert_eq!(v, Version::new("1.2.3".to_string()));
     }
 
     #[test]
     fn test_version_ne() {
-        let v = Version { s: "5.4.3" };
-        assert_ne!(v, Version { s: "5.4.4" });
+        let v = Version::new("5.4.3".to_string());
+        assert_ne!(v, Version::new("5.4.4".to_string()));
     }
 
     #[test]
     fn test_version_lt() {
-        let v = Version { s: "5.4.3" };
-        assert!(v.lt(&Version { s: "5.4.4" }));
-        assert!(!v.lt(&Version { s: "5.4.3" }));
+        let v = Version::new("5.4.3".to_string());
+        assert!(v.lt(&Version::new("5.4.4".to_string())));
+        assert!(!v.lt(&Version::new("5.4.3".to_string())));
     }
 
     #[test]
     fn test_version_gt() {
-        let v = Version { s: "5.4.4" };
-        assert!(v.gt(&Version { s: "5.4.3" }));
-        assert!(!v.gt(&Version { s: "5.4.4" }));
+        let v = Version::new("5.4.4".to_string());
+        assert!(v.gt(&Version::new("5.4.3".to_string())));
+        assert!(!v.gt(&Version::new("5.4.4".to_string())));
     }
 
     #[test]
     fn test_version_le() {
-        let v = Version { s: "5.4.3" };
-        assert!(v.le(&Version { s: "5.4.4" }));
-        assert!(v.le(&Version { s: "5.4.3" }));
+        let v = Version::new("5.4.3".to_string());
+        assert!(v.le(&Version::new("5.4.4".to_string())));
+        assert!(v.le(&Version::new("5.4.3".to_string())));
     }
 
     #[test]
     fn test_version_ge() {
-        let v = Version { s: "5.4.4" };
-        assert!(v.ge(&Version { s: "5.4.3" }));
-        assert!(v.ge(&Version { s: "5.4.4" }));
+        let v = Version::new("5.4.4".to_string());
+        assert!(v.ge(&Version::new("5.4.3".to_string())));
+        assert!(v.ge(&Version::new("5.4.4".to_string())));
     }
 }
