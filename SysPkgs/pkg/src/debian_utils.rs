c57e2b1e01f0a67320912a10f35cfa3cfe18589e
Derek Leggo <dleggo@protonmail.com>
2024-11-21 17:11:39 -0700

    Add structs, and more work

--- SysPkgs/pkg/src/debian_utils.rs
+++ SysPkgs/pkg/src/debian_utils.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 use crate::system;
 use std::process::ExitStatus;
 
-struct DebPkg {
+pub struct DebPkg {
 	name: String,
 	version: String,
 	arch: String,
@@ -12,15 +12,41 @@ struct DebPkg {
 	path: Path
 }
 
+pub struct DebFile<'a> {
+	path: &'a Path
+}
+
+impl DebFile<'_> {
+	pub fn new(path: &str) -> DebFile {
+		DebFile {
+			path: Path::new(path)
+		}
+	}
+}
+
+/// Extracts the full contents of a .deb package to the specified directory.
+///
+/// This function uses the `dpkg-deb` command to extract the entire contents of
+/// the provided .deb package (`d`) into the directory specified by `out`.
+///
+/// # Arguments
+///
+/// - `d` - A reference to a `DebPkg` struct representing the .deb package to extract.
+/// - `out` - A reference to a `Path` specifying the output directory where the package contents will be extracted.
+///
+/// # Returns
+///
+/// A boolean indicating the success (`true`) or failure (`false`) of the extraction process.
 pub fn extract_deb_full(d: &DebPkg, out: &Path) -> bool {
 	system::cmd(&format!("dpkg-deb -R {} {}", d.path.display(), out.display()))
 }
+
 
 pub fn extract_deb(d: &DebPkg, out: &Path) -> bool {
 	system::cmd(&format!("dpkg-deb -R {} {}", d.path.display(), out.display()))
 }
 
-///
-pub fn extract_info(path: &str) -> DebPkg {
-
+/// Gets a DebPkg struct from a deb file
+pub fn extract_info(deb: &DebFile) {
+	
 }
--- SysPkgs/pkg/src/main.rs
+++ SysPkgs/pkg/src/main.rs
@@ -1,4 +1,7 @@
-#[allow(unused_imports)] // ;)
+#![allow(unused_imports)] // ;D
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
 use log::{error, warn, info, debug};
 
 mod version;
